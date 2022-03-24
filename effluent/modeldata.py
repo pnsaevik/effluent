32be8f6263eb70de8c2c5c2ea9a87c8714cdf874
Pål Næverlid Sævik <a5606@hi.no>
2022-03-24 08:35:39 +0100

    Add roms model class

--- effluent/modeldata.py
+++ effluent/modeldata.py
@@ -88,11 +88,90 @@ class Modeldata:
         pass
 
     @staticmethod
-    def from_single_profile(z, dens, u=None, v=None, w=None, lat=None, lon=None, date=None):
+    def from_profile(z, dens, u=None, v=None, w=None, lat=None, lon=None, date=None):
         return ModeldataSingleProfile(z, dens, u, v, w, lat, lon, date)
+
+    @staticmethod
+    def from_roms(file_names):
+        return ModeldataRoms(file_names)
 
     def profile(self, lat, lon, date):
         raise NotImplementedError("Use from_* methods to generate an instance")
+
+
+# --- ROMS functions ---
+
+class ModeldataRoms(Modeldata):
+    def __init__(self, file_names):
+        super().__init__()
+        self.filetab = _filetab_create(file_names)
+        self.horizontal_transform = _roms_horizontal_transform(file_names[0])
+
+    def profile(self, lat, lon, date):
+
+        pass
+
+    def _from_date_to_index(self, date):
+        dates = self.filetab.time_val.values
+        i = np.searchsorted(dates, date)
+        row = self.filetab.iloc[i]
+        return row.file_index, row.time_index
+
+
+def _roms_horizontal_transform(file_name):
+    from .utils import bilin_inv
+
+    with xr.open_dataset(file_name) as dset:
+        lat_rho = dset.variables['lat_rho'].values
+        lon_rho = dset.variables['lon_rho'].values
+
+    previous_latlon = (None, None)
+    previous_xy = [None, None]
+
+    def latlon_to_xy(lat, lon):
+        if previous_latlon != (lat, lon):
+            x, y = bilin_inv(lat, lon, lat_rho, lon_rho)
+            previous_xy[0] = x
+            previous_xy[1] = y
+        return previous_xy
+
+    return latlon_to_xy
+
+
+def _filetab_create(file_names):
+    if isinstance(file_names, str):
+        file_names = [file_names]
+
+    import pandas as pd
+
+    file_index_series = []
+    file_name_series = []
+    time_index_series = []
+    time_val_series = []
+    for i, fname in enumerate(file_names):
+        with xr.open_dataset(fname) as dset:
+            t = dset.variables['ocean_time'].values.astype('datetime64[us]').tolist()
+        time_val_series += t
+        time_index_series += list(range(len(t)))
+        file_name_series += [fname] * len(t)
+        file_index_series += [i] * len(t)
+
+    df = pd.DataFrame(
+        dict(
+            file_name=file_name_series,
+            file_index=file_index_series,
+            time_val=time_val_series,
+            time_index=time_index_series,
+        )
+    )
+
+    df.sort_values(by='time_val', inplace=True)
+    df.drop_duplicates(subset='time_val', inplace=True)
+
+    return df
+
+
+# --- End ROMS functions
 
 
 class ModeldataSingleProfile(Modeldata):
--- tests/test_modeldata.py
+++ tests/test_modeldata.py
@@ -1,9 +1,10 @@
 from effluent import modeldata
+import numpy as np
 
 
 class Test_from_profile:
     def test_returns_dataset_with_standardized_names(self):
-        m = modeldata.Modeldata.from_single_profile(
+        m = modeldata.Modeldata.from_profile(
             z=[0, 10, 50],
             u=[.3, .2, .1],
             v=[.03, .02, .01],
@@ -13,3 +14,42 @@ class Test_from_profile:
         p = m.profile()
 
         assert set(p.variables) == {'z', 'dens', 'u', 'v', 'w', 'lat', 'lon', 'date'}
+
+
+class Test_filetab_create:
+    def test_returns_merged_table_of_times(self):
+        from pathlib import Path
+        root = Path(__file__).parent
+        file_names = [str(root / f) for f in ['forcing_1.nc', 'forcing_2.nc']]
+        df = modeldata._filetab_create(file_names)
+        assert df.file_index.tolist() == [0, 0, 0, 0, 1, 1, 1, 1]
+        assert df.file_name.tolist() == [file_names[0]] * 4 + [file_names[1]] * 4
+        assert df.time_index.tolist() == [0, 1, 2, 3, 0, 1, 2, 3]
+        assert df.time_val.values.astype('datetime64[h]').astype(str).tolist() == [
+            '2015-09-07T01', '2015-09-07T02', '2015-09-07T03', '2015-09-07T04',
+            '2015-09-07T05', '2015-09-07T06', '2015-09-07T07', '2015-09-07T08',
+        ]
+
+
+class Test_horizontal_transform:
+    def test_returns_transform_from_latlon_to_ji(self):
+        from pathlib import Path
+        root = Path(__file__).parent
+        file_name = str(root / 'forcing_1.nc')
+        horz_trans = modeldata._roms_horizontal_transform(file_name)
+        j, i = horz_trans(59.02619, 5.67831)
+        assert i.round(2) == 3.00
+        assert j.round(2) == 2.00
+
+
+class Test_from_date_to_index:
+    def test_returns_file_and_time_index(self):
+        from pathlib import Path
+        root = Path(__file__).parent
+        file_names = [str(root / f) for f in ['forcing_1.nc', 'forcing_2.nc']]
+
+        m = modeldata.ModeldataRoms(file_names)
+        fidx, tidx = m._from_date_to_index(np.datetime64('2015-09-07T07'))
+
+        assert fidx == 1
+        assert tidx == 2
