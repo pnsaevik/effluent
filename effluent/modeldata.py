edc2d9d3a90c8cd535699b127aa5938d1ec9c791
Pål Næverlid Sævik <a5606@hi.no>
2022-03-25 11:18:50 +0100

    Add seawater density formula

--- effluent/modeldata.py
+++ effluent/modeldata.py
@@ -142,7 +142,7 @@ class ModeldataRoms(Modeldata):
             temp = np.flip(ddset.temp.values)
             z = -np.flip(_roms_get_zrho(ddset))
 
-        dens = 1026 - 1.6e-1 * temp + 7.7e-1 * salt
+        dens = _seawater_density(temp, salt, z)
 
         return _create_profile(z, dens, u, v, w, lat, lon, date)
 
@@ -163,6 +163,31 @@ def _roms_open(file_name, *args, **kwargs):
             yield dset
     except ValueError:
         yield file_name
+
+
+def _seawater_density(temp, salt, depth):
+    """Simplified equation of state, from the NEMO documentation"""
+
+    rho_0 = 1026         # seawater reference density
+    a0 = 1.6550e-1       # linear thermal expansion coeff.
+    b0 = 7.6554e-1       # linear haline expansion coeff.
+    lambda1 = 5.9520e-2  # cabbeling coeff. in T^2
+    lambda2 = 5.4914e-4  # cabbeling coeff. in S^2
+    nu = 2.4341e-3       # cabbeling coeff. in T S
+    mu1 = 1.4970e-4      # thermobaric coeff. in T
+    mu2 = 1.1090e-5      # thermobaric coeff. in S
+
+    t_a = temp - 10
+    s_a = salt - 35
+    z = depth
+
+    da_times_rho0 = (
+            -a0 * (1 + 0.5 * lambda1 * t_a + mu1 * z) * t_a
+            + b0 * (1 - 0.5 * lambda2 * s_a - mu2 * z) * s_a
+            - nu * t_a * s_a
+    )
+
+    return rho_0 + da_times_rho0
 
 
 def _roms_get_zrho(dset):
