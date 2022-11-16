545472714f26c72e62500ba3e9cb673cafce22ff
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-11-16 08:05:13 +0100

    Extract methods

--- effluent/script.py
+++ effluent/script.py
@@ -1,14 +1,32 @@
 def run(*argv):
+    import xarray as xr
     conf_fname = argv[0]
 
-    # Load config file
+    conf = load_config(conf_fname)
+    result = xr.Dataset()
+    save_array(result, conf['output']['file'])
+
+    return conf['output']['file']
+
+
+def load_config(fname):
     import yaml
-    with open(conf_fname, encoding='utf-8') as fp:
+    with open(fname, encoding='utf-8') as fp:
         conf = yaml.safe_load(fp)
 
-    # Save output file
-    import xarray as xr
-    result_fname = conf['output']['file']
-    xr.Dataset().to_netcdf(result_fname)
+    return conf
 
-    return result_fname
+
+def save_array(darr, fname):
+    from pathlib import Path
+    suffix = Path(fname).suffix
+
+    if suffix == '.csv':
+        with open(fname, 'w', encoding='utf-8', newline='\n') as fp:
+            from .utils import xr_to_csv
+            xr_to_csv(darr, fp)
+
+    # Default file format: NetCDF4
+    else:
+        #darr.to_dataset().to_netcdf(fname)
+        darr.to_netcdf(fname)
