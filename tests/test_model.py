18a2ba8c02b840a0e9e0dda3315bcd8ad19b7b2e
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-11-18 14:46:52 +0100

    Add test for result_horz_coflow scenario

--- tests/test_model.py
+++ tests/test_model.py
@@ -188,19 +188,19 @@ class Test_Ambient_from_config:
 
 
 class Test_IVP_solve:
-    @pytest.fixture()
+    @pytest.fixture(scope='class')
     def pipe_dset_horz(self):
         return xr.Dataset(dict(depth=100, u=1, w=0, dens=1000, diam=0.5))
 
-    @pytest.fixture()
+    @pytest.fixture(scope='class')
     def pipe_dset_light(self):
         return xr.Dataset(dict(depth=100, u=1, w=0, dens=990, diam=0.5))
 
-    @pytest.fixture()
+    @pytest.fixture(scope='class')
     def pipe_dset_decl(self):
         return xr.Dataset(dict(depth=100, u=1, w=1, dens=1000, diam=0.5))
 
-    @pytest.fixture()
+    @pytest.fixture(scope='class')
     def ambient_dset_still(self):
         return xr.Dataset(
             data_vars=dict(
@@ -211,7 +211,7 @@ class Test_IVP_solve:
             coords=dict(depth=[0, 1000]),
         )
 
-    @pytest.fixture()
+    @pytest.fixture(scope='class')
     def ambient_dset_cross(self):
         return xr.Dataset(
             data_vars=dict(
@@ -222,7 +222,18 @@ class Test_IVP_solve:
             coords=dict(depth=[0, 1000]),
         )
 
-    @pytest.fixture()
+    @pytest.fixture(scope='class')
+    def ambient_dset_coflow(self):
+        return xr.Dataset(
+            data_vars=dict(
+                u=xr.Variable('depth', [0.5, 0.5]),
+                v=xr.Variable('depth', [0, 0]),
+                dens=xr.Variable('depth', [1000, 1000]),
+            ),
+            coords=dict(depth=[0, 1000]),
+        )
+
+    @pytest.fixture(scope='class')
     def result_horz_still(self, pipe_dset_horz, ambient_dset_still):
         """
         Scenario: Horizontal pipe with constant output, no ambient velocity or
@@ -232,7 +243,7 @@ class Test_IVP_solve:
         ivp = model.InitialValueProblem(steps, pipe_dset_horz, ambient_dset_still)
         return ivp.solve()
 
-    @pytest.fixture()
+    @pytest.fixture(scope='class')
     def result_decl_still(self, pipe_dset_decl, ambient_dset_still):
         """
         Scenario: Horizontal pipe with constant output, no ambient velocity or
@@ -242,7 +253,7 @@ class Test_IVP_solve:
         ivp = model.InitialValueProblem(steps, pipe_dset_decl, ambient_dset_still)
         return ivp.solve()
 
-    @pytest.fixture()
+    @pytest.fixture(scope='class')
     def result_horz_cross(self, pipe_dset_horz, ambient_dset_cross):
         """
         Scenario: Horizontal pipe with constant output, no ambient velocity or
@@ -252,7 +263,17 @@ class Test_IVP_solve:
         ivp = model.InitialValueProblem(steps, pipe_dset_horz, ambient_dset_cross)
         return ivp.solve()
 
-    @pytest.fixture()
+    @pytest.fixture(scope='class')
+    def result_horz_coflow(self, pipe_dset_horz, ambient_dset_coflow):
+        """
+        Scenario: Horizontal pipe with constant output, no ambient velocity or
+        density difference.
+        """
+        steps = np.array([0, 10, 20])
+        ivp = model.InitialValueProblem(steps, pipe_dset_horz, ambient_dset_coflow)
+        return ivp.solve()
+
+    @pytest.fixture(scope='class')
     def result_light_still(self, pipe_dset_light, ambient_dset_still):
         """
         Scenario: Horizontal pipe with constant output, no ambient velocity or
@@ -316,3 +337,19 @@ class Test_IVP_solve:
         assert self.sign_of_change(r, 'w') == -1
         assert self.sign_of_change(r, 'density') == 0
         assert self.sign_of_change(r, 'radius') == 1
+
+    def test_variables_change_as_expected_when_horz_coflow(self, result_horz_coflow):
+        r = result_horz_coflow
+        assert self.sign_of_change(r, 'x') == 1
+        assert self.sign_of_change(r, 'y') == 0
+        assert self.sign_of_change(r, 'z') == 0
+        assert self.sign_of_change(r, 'u') == -1
+        assert self.sign_of_change(r, 'v') == 0
+        assert self.sign_of_change(r, 'w') == 0
+        assert self.sign_of_change(r, 'density') == 0
+        assert self.sign_of_change(r, 'radius') == 1
+
+    def test_coflow_jet_travels_longer(self, result_horz_coflow, result_horz_still):
+        x_coflow = result_horz_coflow.x.values
+        x_still = result_horz_still.x.values
+        assert x_coflow[-1] > x_still[-1]
