cdd6d6146c2514911307c99198e6b26834a7d8c2
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2023-06-12 08:12:33 +0200

    Remove main method from package root

--- src/effluent/__init__.py
+++ src/effluent/__init__.py
@@ -1,7 +1,8 @@
 """
-This is a package docstring.
+The main package contains only a single function, :func:`effluent.run`, which is the
+main entry point of the application.
 """
 
-from .script import run, main
+from .script import run
 
 __version__ = "1.2"
--- src/effluent/__main__.py
+++ src/effluent/__main__.py
@@ -1,3 +1,3 @@
-from . import main
+from .script import main
 
 main()
